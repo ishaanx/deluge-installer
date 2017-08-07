5fd41581ba827af56682d12c0057d56f6a34d0f1
e-sean <b.ishan@protonmail.com>
2017-08-07 18:56:03 +0530

    Update deluge.sh

--- deluge.sh
+++ deluge.sh
@@ -2,13 +2,13 @@
 
 #sudo apt-get install python-software-properties -y 
 
-yes "" | sudo add-apt-repository ppa:deluge-team/ppa
+#yes "" | sudo add-apt-repository ppa:deluge-team/ppa
 
 #sudo apt update -y
 
 #sudo apt upgrade -y
 
-sudo apt install deluged deluge-web -y 
+#sudo apt install deluged deluge-web -y 
 ###########DELUGE USER ##############
 
 sudo adduser --system  --gecos "Deluge Service" --disabled-password --group --home /var/lib/deluge deluge
@@ -19,22 +19,25 @@ sudo rm deluged.service
 wget https://raw.githubusercontent.com/e-sean/deluge/master/deluged.service
 sudo cp deluged.service /etc/systemd/system/
 
+sudo systemctl daemon-reload
 sudo systemctl start deluged
-sudo systemctl enable /etc/systemd/system/deluged.service
+sudo systemctl enable deluged.service
 
 
 ########### WEB-UI SERVICE ##########
+
 
 sudo rm deluge-web.service
 wget https://raw.githubusercontent.com/e-sean/deluge/master/deluge-web.service
 sudo cp deluge-web.service /etc/systemd/system/
 
+sudo systemctl daemon-reload
 sudo systemctl start deluge-web
-sudo systemctl enable /etc/systemd/system/deluge-web.service
+sudo systemctl enable deluge-web.service
 
 ############# OPEN PORT###############
 
-iptables -I INPUT -p tcp --dport 8112 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 8112 -j ACCEPT
 
 #####kill deluge #########
 killall deluged
@@ -42,5 +45,10 @@ killall deluge-web
 
 ########User and password#############
 read -p 'Enter a username for Deluge remote connection: ' usr
-read -sp 'Enter a new password: ' pass
+read -sp 'Enter a password for $usr: ' pass
 echo "$usr:$pass:10" >> /var/lib/deluge/.config/deluge/auth
+
+########Allow remote ############
+sudo sed -i 's/"allow_remote": false/"allow_remote": true/g' /var/lib/deluge/.config/deluge/core.conf
+
+sudo sed -i 's/"default_daemon": "",/"default_daemon": "127.0.0.1:58846",/g' /var/lib/deluge/.config/deluge/web.conf
